8712645725c2cdc6706914d5bd15db1fa6236c38
James Gadoury <gadouryjames@gmail.com>
2023-11-30 23:39:15 -0500

    Clean up code, add args

--- generate_maze.py
+++ generate_maze.py
@@ -1,62 +1,69 @@
-# Create an SDF string based on the maze
-sdf_content = f"""<?xml version="1.0" ?>
-<sdf version="1.6">
-  <world name="default">
-    <!-- Define a simple ground plane -->
-    <include>
-      <uri>model://ground_plane</uri>
-    </include>
-"""
+from argparse import ArgumentParser
 
-pairs = []
+def main(args):
 
-with open("input_maze.txt") as f:
-    lines = [line.rstrip() for line in f.readlines()]
+    # Create an SDF string based on the maze
+    sdf_content = f"""<?xml version="1.0" ?>
+    <sdf version="1.6">
+    <world name="default">
+        <!-- Define a simple ground plane -->
+        <include>
+        <uri>model://ground_plane</uri>
+        </include>
+    """
 
-y = 0
-for line in lines:
-    print(line)
-for line in lines:
-    x = 0
-    for c in line:
-        if c == "x":
-            pairs.append((x,y))
-            sdf_content += f"""
-                <model name="maze_wall_{x}_{y}">
-                <static>true</static>
-                <link name="link">
-                    <collision name="collision">
-                    <geometry>
-                        <box>
-                        <size>1 1 1</size>
-                        </box>
-                    </geometry>
-                    </collision>
-                    <visual name="visual">
-                    <geometry>
-                        <box>
-                        <size>1 1 1</size>
-                        </box>
-                    </geometry>
-                    <material>
-                        <script>
-                        <uri>file://media/materials/scripts/gazebo.material</uri>
-                        <name>Gazebo/White</name>
-                        </script>
-                    </material>
-                    </visual>
-                </link>
-                <pose>{x} {y} 0 0 0 0</pose>
-                </model>
-            """
-        x += 1
-    y -= 1
+    with open(args.input_file) as f:
+        lines = [line.rstrip() for line in f.readlines()]
 
-sdf_content += """
-  </world>
-</sdf>
-"""
-print(pairs)
-# Save the SDF content to a file
-with open("maze_world.sdf", "w") as sdf_file:
-    sdf_file.write(sdf_content)
+    y = 0
+    for line in lines:
+        x = 0
+        for c in line:
+            if c == "x":
+                sdf_content += f"""
+                    <model name="maze_wall_{x}_{y}">
+                    <static>true</static>
+                    <link name="link">
+                        <collision name="collision">
+                        <geometry>
+                            <box>
+                            <size>1 1 {args.wall_height}</size>
+                            </box>
+                        </geometry>
+                        </collision>
+                        <visual name="visual">
+                        <geometry>
+                            <box>
+                            <size>1 1 {args.wall_height}</size>
+                            </box>
+                        </geometry>
+                        <material>
+                            <script>
+                            <uri>file://media/materials/scripts/gazebo.material</uri>
+                            <name>Gazebo/White</name>
+                            </script>
+                        </material>
+                        </visual>
+                    </link>
+                    <pose>{x} {y} 0 0 0 0</pose>
+                    </model>
+                """
+            x += 1
+        y -= 1
+
+    sdf_content += """
+    </world>
+    </sdf>
+    """
+    # Save the SDF content to a file
+    with open(args.output_file, "w") as sdf_file:
+        sdf_file.write(sdf_content)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--input-file", default="input_maze.txt")
+    parser.add_argument("--output-file", default="maze_world.sdf")
+    parser.add_argument("--wall-height", type=int, default=3)
+
+    main(parser.parse_args())
